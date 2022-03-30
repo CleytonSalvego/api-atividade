d17b707a4e0891a05d108ef129c8433c5947a9ce
Cleyton <cleytonssantos@yahoo.com.br>
2022-03-30 09:49:59 -0300

    Finalização do projeto

--- Configuration.cs
+++ Configuration.cs
@@ -2,10 +2,10 @@
 {
     public static class Configuration
     {
-        public static string JwtKey { get; set; } = "jagejVapc0qQXYl+UApb+Q==";
+        public static string JwtKey { get; set; } = "JwtKey"; //Colocar um hash seguro
 
-        public static string PasswordKey { get; set; } = "123";
+        public static string PasswordKey { get; set; } = "123"; //Colocar uma senha padrão
 
-        public static string ConnectionString { get; set; } = "Server=localhost;DataBase=api-atividade;Uid=root;Pwd=";
+        public static string ConnectionString { get; set; } = "Server=localhost;DataBase=api-atividade;Uid=root;Pwd="; //Colocar a conection string do banco 
     }
 }
